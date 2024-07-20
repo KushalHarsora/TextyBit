d78924666aed9794ae02559a85b6a329d675a816
Kushal Harsora <kushalharsora13@gmail.com>
2024-07-20 20:31:36 +0530

    Login and Register Frontend Done

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -17,26 +17,30 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { useRouter } from 'next/navigation';
 
-const loginSchema = z.object({
+const registerSchema = z.object({
+    username: z.string().min(2, {
+        message: "username required"
+    }),
     email: z.string().email(),
     password: z.string().min(8, {
-        message: "enter password"
+        message: "weak password"
     })
 });
 
-const Login = () => {
+const Register = () => {
 
     const router = useRouter();
 
-    const form = useForm<z.infer<typeof loginSchema>>({
-        resolver: zodResolver(loginSchema),
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
         defaultValues: {
+            username: "",
             email: "",
             password: ""
         },
     });
 
-    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
         console.log(values)
     }
 
@@ -46,7 +50,7 @@ const Login = () => {
             {/* Navbar Laptop */}
 
             <section className=" h-[7.5vh] w-screen fixed top-0 left-0 z-20 max-lg:hidden flex flex-row justify-between items-center px-12 overflow-hidden backdrop-blur-md border-b-[0.5px] border-gray-600">
-                <Image src={"logo.svg"} alt="logo" priority width={120} height={120} className=' cursor-pointer' onClick={() => router.push("/")} />
+                <Image src={"logo.svg"} alt="logo" priority width={120} height={120} />
                 <div className=" w-[15vw] h-full flex flex-row justify-around items-center">
                     <Button
                         variant={'default'}
@@ -68,10 +72,10 @@ const Login = () => {
             {/* Navbar Laptop End */}
             <div className=' flex flex-col justify-center items-center gap-1'>
                 <h1 className=' text-3xl max-lg:text-2xl font-semibold'>
-                    Welcome Back
+                    Welcome To TextyBit
                 </h1>
                 <h2 className=' text-sm max-lg:text-xs font-medium text-gray-400'>
-                    Login into your account
+                    Sign up for an account
                 </h2>
             </div>
             <Form {...form}>
@@ -79,6 +83,18 @@ const Login = () => {
                     onSubmit={form.handleSubmit(onSubmit)}
                     className="space-y-4 max-lg:space-y-6 w-[25vw] max-w-[30vw] max-lg:max-w-[300px] max-lg:w-3/5 max-lg:min-w-[250px]"
                 >
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input type='text' className=' w-full h-full px-3 py-2 border-[0.125px] border-gray-400' placeholder="John Doe" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
                     <FormField
                         control={form.control}
                         name="email"
@@ -104,18 +120,18 @@ const Login = () => {
                         )}
                     />
                     <Button className=' w-full bg-white text-black text-sm hover:bg-slate-200' type='submit'>
-                        Login
+                        Register
                     </Button>
 
                 </form>
             </Form>
             <div>
-                <Link href={'/register'}>
-                    <Button variant={'link'} className=' text-white text-sm'>Don&apos;t have an account? Sign Up</Button>
+                <Link href={'/login'}>
+                    <Button variant={'link'} className=' text-white text-sm'>Already have an account? Sign In</Button>
                 </Link>
             </div>
         </main>
     )
 }
 
-export default Login
+export default Register
