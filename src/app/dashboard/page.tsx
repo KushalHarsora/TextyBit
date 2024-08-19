abacc18c33ccd9c193f129dc591e8ae0f20fcf57
Kushal Harsora <kushalharsora13@gmail.com>
2024-08-19 13:08:36 +0530

    Navbar Done

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+// import Dashboard from '@/components/Dashboard'
+// import { db } from '@/db'
+// import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user) redirect('/auth-callback?origin=dashboard')
+
+  return <div>{user?.email}</div>
+
+//   const dbUser = await db.user.findFirst({
+//     where: {
+//       id: user.id
+//     }
+//   })
+
+//   if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+//   const subscriptionPlan = await getUserSubscriptionPlan()
+
+//   return <Dashboard subscriptionPlan={subscriptionPlan} />
+}
+
+export default Page
