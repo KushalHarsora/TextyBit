a5d0fd6389fa1cc8b0e289ee3b25bad2fa2735a0
Kushal Harsora <kushalharsora13@gmail.com>
2024-08-27 16:50:25 +0530

    Some changes

--- src/app/api/auth/trpc/[trpc]/route.ts
+++ src/app/api/auth/trpc/[trpc]/route.ts
@@ -1,13 +1,12 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '@/trpc';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { appRouter } from '@/trpc'
 
-function handler(req: Request) {
-  return fetchRequestHandler({
+const handler = (req: Request) =>
+  fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: () => ({})
-  });
-}
+    createContext: () => ({}),
+  })
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,15 +1,39 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from "@/db"
+
+type User = {
+  id: String
+  email: String
+}
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
-    const user = await getUser();
+    const user = await getUser() as User;
 
     if(!user?.id || !user?.email) throw new TRPCError({code: "UNAUTHORIZED"});
 
+    const userId = user.id.toString();
+    const userEmail = user.email.toString();
+
     // check user in database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId
+      }
+    });
+
+    if(!dbUser) {
+      // user does not exists
+      await db.user.create({
+        data: {
+          id: userId,
+          email: userEmail,
+        }
+      })
+    }
 
     return { success: true }
   })
