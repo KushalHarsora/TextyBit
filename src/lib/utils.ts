cd18b6e90a9d9d1ffdc6f6ce6b280cea60b2ad7d
Kushal Harsora <kushalharsora13@gmail.com>
2024-10-29 11:50:24 +0530

    Restart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,60 +1,6 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function absoluteUrl(path: string) {
-  if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${
-    process.env.PORT ?? 3000
-  }${path}`
-}
-
-export function constructMetadata({
-  title = "TextyBit - Chat with your Documents",
-  description = "extyBit is an open-source software to make chatting to your PDF files easy.",
-  image = "/thumbnail.png",
-  icons = "/favicon.ico",
-  noIndex = false
-}: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
-} = {}): Metadata {
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      images: [
-        {
-          url: image
-        }
-      ]
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [image],
-      creator: "@KushalHarsora"
-    },
-    icons,
-    metadataBase: new URL('https://textybit.vercel.app/'),
-    themeColor: '#FFF',
-    ...(noIndex && {
-      robots: {
-        index: false,
-        follow: false
-      }
-    })
-  }
 }
