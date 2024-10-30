379313828bc323c3d77c7f2885a1dc1a4fa64f7f
Kushal Harsora <kushalharsora13@gmail.com>
2024-10-30 13:13:02 +0530

    input type casting done

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,7 +3,9 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    type?: "text" | "email" | "password" | "number";
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -21,5 +23,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   }
 )
 Input.displayName = "Input"
-
 export { Input }
