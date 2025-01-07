580504546498a40cc9c1d46e223be1cda5afaa87
Kushal Harsora <kushalharsora13@gmail.com>
2025-01-07 22:49:45 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Toaster richColors />
         {children}
+        <Analytics />
       </body>
     </html>
   );
